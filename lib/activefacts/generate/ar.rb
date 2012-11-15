f27f4d2d8faaf67436326f8a9fd9575c5a4aee45
David Parry <david.parry@suranyami.com>
2012-11-15 16:56:53 +1100

    first draft active_record generator

---
+++ lib/activefacts/generate/ar.rb
@@ -0,0 +1,116 @@
+#
+#       ActiveFacts Generators.
+#       Generate ActiveRecord models and migrations from an ActiveFacts vocabulary.
+#
+# Copyright (c) 2012 David Parry. Read the LICENSE file.
+#
+require 'activefacts/vocabulary'
+require 'activefacts/persistence'
+require 'active_record'
+require 'active_support'
+require 'active_support/all'
+
+module ActiveFacts
+  module Generate
+    # Generate ActiveRecord models and migrations for an ActiveFacts vocabulary.
+    # Invoke as
+    #   afgen --ar[=options] <file>.cql
+    # Options are comma or space separated:
+    class AR
+      include Persistence
+
+      def initialize(vocabulary, *options)
+        @vocabulary = vocabulary
+        @vocabulary = @vocabulary.Vocabulary.values[0] if ActiveFacts::API::Constellation === @vocabulary
+      end
+
+      def puts s
+        @out.puts s
+      end
+
+      def go s
+        puts s + ";\n\n"
+      end
+
+      public
+      def generate(out = $>)
+        @out = out
+        @vocabulary.tables.each do |table|
+          migration table do
+            table.columns.each do |column|
+              type, params, constraints = column.type
+              field(column.name, type)
+            end
+          end
+        end
+      end
+
+      private
+
+      def field(name, type)
+        type = normalise_type(type)
+        <<-HERE
+            t.#{type} "#{name}"
+        HERE
+      end
+
+      def normalise_type(type, length = 0)
+        case type
+        when /^Auto ?Counter$/
+          'integer'
+
+        when /^Unsigned ?Integer$/,
+          /^Signed ?Integer$/,
+          /^Unsigned ?Small ?Integer$/,
+          /^Signed ?Small ?Integer$/,
+          /^Unsigned ?Tiny ?Integer$/
+          'integer'
+
+        when /^Decimal$/
+          'decimal'
+
+        when /^Fixed ?Length ?Text$/, /^Char$/
+          'string'
+        when /^Variable ?Length ?Text$/, /^String$/
+          'string'
+        when /^Large ?Length ?Text$/, /^Text$/
+          'text'
+
+        when /^Date ?And ?Time$/, /^Date ?Time$/
+          'datetime'
+        when /^Date$/
+              'datetime' # SQLSVR 2K5: 'date'
+        when /^Time$/
+          'datetime' # SQLSVR 2K5: 'time'
+        when /^Auto ?Time ?Stamp$/
+          'timestamp'
+
+        when /^Money$/
+          'decimal'
+        when /^Picture ?Raw ?Data$/, /^Image$/
+          'blob'
+        when /^Variable ?Length ?Raw ?Data$/, /^Blob$/
+          'blob'
+        when /^BIT$/
+          'bit'
+        else
+          type
+        end
+      end
+
+      def migration(table)
+        <<-HERE
+        class Create#{table.name.capitalize.pluralize} < ActiveRecord::Migration
+          def change
+            create_table #{table.name.tableize} do |t|
+              #{yield}
+              t.timestamps
+            end
+          end
+        end
+
+        HERE
+      end
+    end
+  end
+end
